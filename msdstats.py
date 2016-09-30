98073fcbdf376f0e3588f87408431e77ca91c9b3
Mario Burbano <burbano.carmona@gmail.com>
2016-09-30 15:26:29 +0200

    Generates a csv file with filenames as indices and their corresponding statistics

--- msdstats.py
+++ msdstats.py
@@ -6,10 +6,11 @@
 
 from __future__ import print_function
 import numpy as np
+import pandas as pd
 import argparse
 from subprocess import check_output
 import matplotlib
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import sys
 
@@ -17,8 +18,12 @@ def parse_commandline_arguments():
     parser = argparse.ArgumentParser( description = 'msd error analysis' )
     parser.add_argument( '--atoms', '-a', metavar = 'N', type=int, required = True, help='set the number of atoms' )
     parser.add_argument( '--frames', '-f', metavar = 'N', type=int, required = True, help='number of displacement frames in the complete displacement file' )
+    ###########################################################################################################################################
+    # The following two values correspond to tau and delta, respectively in the following article:
+    # http://pubs.acs.org/doi/abs/10.1021/acs.jctc.5b00574?journalCode=jctcce
     parser.add_argument( '--slicesize', '-ss', metavar = 'N', type=int, required = True, help='Size of the slice in number of frames' )
     parser.add_argument( '--offset', '-so', metavar = 'N', type=int, required = True, help='Offset (in number of frames) between the slices' )
+    ############################################################################################################################################
     parser.add_argument( '--executable', '-x', help='msd code executable name', default='msdconf.x' )
     parser.add_argument( '--displacement-file', '-d', help='complete displacement file filename', default='displong.out' )
     parser.add_argument( '--msd-files', '-m', nargs='+', help='list of msd output files to analyse', required = True )
@@ -64,16 +69,20 @@ if __name__ == '__main__':
         for i, filename in enumerate( msd_files ):
             msd_slope[i,j] = get_slope_from_msd_output( filename )
 
-    stats_file = open('stats.dat','w')
-    stats_file.write( '# filename\n' )
-    stats_file.write( '# fileindex mean std nframes\n' )
+    columns = ['Mean','Standard Deviation','sim length','tau','delta','number of slices']
+    stats = pd.DataFrame(columns = columns)
+    #stats_file = open('stats.dat','w')
+    #stats_file.write( '# filename\n' )
+    #stats_file.write( '# fileindex mean std nframes\n' )
     for i, filename in enumerate( msd_files ):
-        stats_file.write( '{}\n'.format( '# '+filename ))
-        stats_file.write( '{} {:f} {:f} {:f}\n'.format(i , np.mean( msd_slope[i] ), np.std( msd_slope[i] ), n_total_disp_frames ))
-        plt.plot(msd_slope[i],'o-',label=filename)
-        plt.legend(loc='best')
-    stats_file.close()
-    np.savetxt( 'slopes.dat', msd_slope )
+	stats.loc[filename] =[ np.mean(msd_slope[i]), np.std(msd_slope[i]), n_total_disp_frames, n_slicesize_frames, delta_slicesize_start, number_of_slices ]
+        #stats_file.write( '{}\n'.format( '# '+filename ))
+        #stats_file.write( '{} {:f} {:f} {:f}\n'.format(i , np.mean( msd_slope[i] ), np.std( msd_slope[i] ), n_total_disp_frames ))
+        #plt.plot(msd_slope[i],'o-',label=filename)
+        #plt.legend(loc='best')
+    stats.to_csv('stats.csv')
+    #stats_file.close()
+    #np.savetxt( 'slopes.dat', msd_slope )
 
-    plt.savefig('slopes.pdf')
+    #plt.savefig('slopes.pdf')
 
