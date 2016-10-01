ecbe2f0c42b68143aa18217b75ba47767ee68cca
Mario Burbano <burbano.carmona@gmail.com>
2016-10-01 12:54:19 +0200

    Added function slope_convergence

--- msdstats.py
+++ msdstats.py
@@ -9,9 +9,8 @@ import numpy as np
 import pandas as pd
 import argparse
 from subprocess import check_output
-#import matplotlib
 #matplotlib.use('Agg')
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import sys
 
 def parse_commandline_arguments():
@@ -28,6 +27,33 @@ def parse_commandline_arguments():
     parser.add_argument( '--displacement-file', '-d', help='complete displacement file filename', default='displong.out' )
     parser.add_argument( '--msd-files', '-m', nargs='+', help='list of msd output files to analyse', required = True )
     return parser.parse_args()
+
+def read_msd_template(template_file='msd_template'):
+    lines = []
+    with open(template_file) as f:
+        for line in f.readlines():
+            lines.append(line)
+    return lines
+
+def write_msd_inpt(template,msd_length,n_frames,nprint=1000,timestep=41.3414,dist_warn=3.5):
+    f = open('msd.inpt','w')
+    for line in template:
+        f.write(line)
+    f.write(str(msd_length) + '\n')
+    f.write('.false.\n')
+    f.write('.true.\n')
+    f.write(str(nprint) + '\n')
+    f.write(str(timestep) + '\n')
+    f.write(str(dist_warn) + '\n')
+    f.write(str(n_frames*nprint) + '\n')
+    f.close()
+
+def plot_slopes(msd_slopes,files_list,tot_sim_length,delta,tau):
+    plt.figure()
+    plt.title(r'\delta = ' + str(delta) + '\n' + r'\tau = ' + str(tau))
+    for i, filename in enumerate( files_list ):
+        plt.plot(msd_slopes[i],'o-',label=filename)
+        plt.legend(loc='best')
 
 def linear_regression( x, y ):
     # http://docs.scipy.org/doc/numpy/reference/generated/numpy.linalg.lstsq.html#numpy.linalg.lstsq
@@ -62,14 +88,40 @@ def slope_statistics( disp_filename, natoms, nframes, slice_size, slice_offset, 
         for i, filename in enumerate( msd_files ):
             msd_slopes[i,j] = get_slope_from_msd_output( filename )
 
-	columns = ['Mean','Standard Deviation','sim length','tau','delta','number of slices']
-	stats = pd.DataFrame( columns = columns, index= msd_files )
-	for i, filename in enumerate( msd_files ):
-	    stats.loc[filename] =[ np.mean(msd_slopes[i]), np.std(msd_slopes[i]), nframes, slice_size, slice_offset, nslices ]
-	stats.to_csv('slope-stats.csv')
+    columns = ['Mean','Standard Deviation','sim length','tau','delta','number of slices']
+    stats = pd.DataFrame( columns = columns, index= msd_files )
+    for i, filename in enumerate( msd_files ):
+        stats.loc[filename] =[ np.mean(msd_slopes[i]), np.std(msd_slopes[i]), nframes, slice_size, slice_offset, nslices ]
+    stats.to_csv('slope-stats.csv')
 
     return msd_slopes
 
+def slope_convergence( disp_filename, natoms, nframes, msd_length, slice_offset, files_to_monitor, msd_executable ):
+# This function analyses how the slopes change as we increase the simulation time. It creates slices of the 
+# displacement file that become increasingly larger. 
+    my_template = read_msd_template('msd_template')
+    
+    temp_disp_filename = 'dispslice.out'
+
+    complete_disp_data = np.loadtxt( disp_filename )
+    complete_disp_data = complete_disp_data.reshape( ( nframes, natoms, 3 ) )
+    slices = range( slice_offset, nframes + slice_offset, slice_offset )
+    nslices = len( slices )
+    msd_slopes = np.empty( ( len( files_to_monitor ), nslices ) )
+    
+    for j, new_length in enumerate( slices ):
+        
+        sliced_disp_data = complete_disp_data[ 0 : new_length ]
+        sliced_disp_data = sliced_disp_data.reshape( new_length * natoms, 3 )
+        np.savetxt( temp_disp_filename, sliced_disp_data )
+        write_msd_inpt(my_template,msd_length=msd_length,n_frames=new_length)
+        out = check_output( [ msd_executable ] )
+        for i, filename in enumerate( files_to_monitor ):
+            msd_slopes[i,j] = get_slope_from_msd_output( filename )
+    columns = slices
+    slopes_df = pd.DataFrame( msd_slopes, index=files_to_monitor, columns=columns )
+    slopes_df.to_csv('slope-conv.csv')
+    return msd_slopes
 
 if __name__ == '__main__':
     args = parse_commandline_arguments()
@@ -81,6 +133,7 @@ if __name__ == '__main__':
     msd_executable = args.executable
     displacements_file = args.displacement_file # displacements without the header information
     msd_files = args.msd_files
-
-    msd_slopes = slope_statistics( displacements_file, natoms, nframes_tot, slice_size_frames, slice_offset, msd_files, msd_executable ) 
+    
+    slope_conv = slope_convergence( displacements_file, natoms, nframes_tot, slice_offset, slice_offset, msd_files, msd_executable )
+    slope_stats = slope_statistics( displacements_file, natoms, nframes_tot, slice_size_frames, slice_offset, msd_files, msd_executable ) 
 
