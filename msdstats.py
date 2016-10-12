02016c310966e62fbc67cee3a456bb6da6a8262d
Mario Burbano <burbano.carmona@gmail.com>
2016-10-12 11:27:29 +0200

    Changed default displacements file name to displong.out.gz

--- msdstats.py
+++ msdstats.py
@@ -36,7 +36,7 @@ def parse_commandline_arguments():
     parser.add_argument( '--msdlen', '-ml', metavar = 'N', type=int, required = True, help='Length of msd' )
     parser.add_argument( '--prntfrq', '-p', metavar = 'N', type=int, required = False, default = 1000, help='Print frequency of displacements' )
     parser.add_argument( '--timestep', '-t', metavar = 'F', type=float, required = False, default = 41.3414, help='Simulation timestep in a.u.' )
-    parser.add_argument( '--displacement-file', '-d', help='complete displacement file filename', default='displong.out' )
+    parser.add_argument( '--displacement-file', '-d', help='complete displacement file filename', default='displong.out.gz' )
     parser.add_argument( '--msd-files', '-m', nargs='+', help='list of msd output files to analyse', required = True )
     parser.add_argument( '--convcalc', '-cc', action='store_true', help='Perform a slope convergence calculation rather than the slope statistics calculation.' )
     return parser.parse_args()
